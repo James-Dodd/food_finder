7f35558809574343dae4e0f91e006bdf076d2e37
Tim Lee <tim.lee@red-badger.com>
2020-08-12 14:16:50 +0100

    Add search food route

--- src/app-express.ts
+++ src/app-express.ts
@@ -1,14 +1,28 @@
 const express = require("express")
-import * as path from "path"
+import FoodFinder from "./services/food-finder"
 
 const app = express()
 const port = 3000
 
+let foodFinder = new FoodFinder()
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the food finder!")
+  res.send("<div>Welcome to the food finder!</div>")
+})
+
+app.get("/file", (req, res) => {
+  res.render("index", {
+    title: "I'm using pug!",
+    message: "Wow this is what pug looks like. It's incredible.",
+  })
+})
+
+app.get("/search/:food", (req, res) => {
+  res.send(foodFinder.findFood(req.params.food))
 })
 
 app.set("title", "Food finder")
+
 app.set("view engine", "pug")
 app.set("views", "./out/views")
 
