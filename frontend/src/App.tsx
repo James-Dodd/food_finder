36f392971e3d6e709aa5750501b4b3cc10ee797a
Tim Lee <tim.lee@red-badger.com>
2020-08-25 14:30:09 +0100

    Rename frontend-ts to frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+interface HeaderProps {
+  name: string
+  age?: number
+}
+
+function Header(props: HeaderProps) {
+  return (
+    <h1>
+      Hello {props.name}. You are {props.age && 99}
+    </h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header name="Harry" age={16} />
+        <Header name="Sally" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
