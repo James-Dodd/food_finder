af760285ae8ddf8de36225b67a990260cbca5815
Tim Lee <tim.lee@red-badger.com>
2020-08-27 13:30:03 +0100

    Remove unused H1

--- frontend/src/components/FoodSearch/index.tsx
+++ frontend/src/components/FoodSearch/index.tsx
@@ -1,10 +1,4 @@
 import React from "react"
-import styled from "styled-components"
-
-const StyledH1 = styled.h1`
-  color: red;
-  font-size: 80px;
-`
 
 export default function FoodSearch() {
   return (
