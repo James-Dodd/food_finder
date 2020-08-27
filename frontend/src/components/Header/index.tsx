321e44fd4edb9eb45b92341b24f7e74bd684121f
Tim Lee <tim.lee@red-badger.com>
2020-08-27 13:29:41 +0100

    Remove unused component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 
-interface MyOwnH1Props {
-  isRed: boolean
-  children: React.ReactNode
-}
-
-function MyOwnH1(props: MyOwnH1Props) {
-  return <h1>{props.children}</h1>
-}
-
 const StyledH1 = styled.h1`
   color: red;
   font-size: 80px;
@@ -24,3 +15,4 @@ interface HeaderProps {
 export default function Header(props: HeaderProps) {
   return <StyledH1>{props.children}</StyledH1>
 }
+
