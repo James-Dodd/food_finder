441886ed5c0683e89a062052b03d96609d8da224
Tim Lee <tim.lee@red-badger.com>
2020-08-28 13:19:07 +0100

    Adding state

--- frontend/src/components/FoodSearch/index.tsx
+++ frontend/src/components/FoodSearch/index.tsx
@@ -1,13 +1,41 @@
-import React from "react"
+import React, { useState, FormEvent } from "react"
 
 export default function FoodSearch() {
+  let [searchedFood, setSearchedFood] = useState("")
+  let [searchedFoods, setSearchedFoods] = useState<string[]>([])
+
+  const updateFood = (e: FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value)
+    setSearchedFood(e.currentTarget.value)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    searchedFoods.push(searchedFood)
+    setSearchedFoods(searchedFoods)
+    setSearchedFood("")
+  }
   return (
-    <form>
-      <label>
-        Search For Food:
-        <input type="text" name="food" id="food" />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search For Food:
+          <input
+            type="text"
+            name="food"
+            id="food"
+            value={searchedFood}
+            onChange={updateFood}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
+        <h2>Searched foods</h2>
+        {searchedFoods.map((food) => (
+          <p>{food}</p>
+        ))}
+      </div>
+    </div>
   )
 }
